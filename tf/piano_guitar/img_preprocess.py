631ffaaaf87c54a5162637c05829452623bc1faf
super <atharva.kench@gmail.com>
2018-01-08 18:20:12 -0500

    combine full data, split into train, validation, test

--- tf/piano_guitar/img_preprocess.py
+++ tf/piano_guitar/img_preprocess.py
@@ -30,12 +30,56 @@ def preprocess(file_paths_list, rots_per_img = 10, crops_per_rot = 5):
                 processed_images.append(i)
                 step += 1
 
+        print('.')
     return processed_images
 
+def split_data(all_data, all_labels, perc_train = 0.72, perc_val = 0.18, perc_test = 0.1):
+    num_data = len(all_data)
+    num_train = int(perc_train * num_data)
+    num_val = int(perc_val * num_data)
+    num_test = int(perc_test * num_data)
 
-def make_data():
-    piano_paths = glob.glob('org_data/guitar/*.jpg')
-    guitar_paths = glob.glob('org_data/piano/*.jpg')
+    curr = 0
+    train_data = all_data[curr : num_train]
+    train_labels = all_labels[curr : num_train]
+    pickle.dump(train_data, open('processed_data/train_data.p', 'wb'))
+    pickle.dump(train_labels, open('processed_data/train_labels.p', 'wb'))
+
+    curr += num_train
+    val_data = all_data[curr : num_val]
+    val_labels = labels[curr : num_val]
+    pickle.dump(val_data, open('processed_data/val_data.p', 'wb'))
+    pickle.dump(val_labels, open('processed_data/val_labels.p', 'wb'))
+
+    curr += num_val
+    test_data = all_data[curr:]
+    test_labels = all_labels[curr:]
+    pickle.dump(test_data, open('processed_data/test_data.p', 'wb'))
+    pickle.dump(test_labels, open('processed_data/test_labels.p', 'wb'))
+
+def make_full_data():
+
+    piano_norm = pickle.load(open('processed_data/piano_data.p', 'rb'))
+    guitar_norm = pickle.load(open('processed_data/guitar_data.p', 'rb'))
+    print('piano', len(piano_norm))
+    print('guitar', len(guitar_norm))
+
+    all_data = list(piano_norm) + list(guitar_norm)
+
+    all_labels = list(np.full(len(piano_norm), 0)) + list(np.full(len(guitar_norm), 1))
+
+    from sklearn.utils import shuffle
+    all_data, all_labels = shuffle(all_data, all_labels)
+
+    return all_data, all_labels
+
+
+    print('Success! :)')
+
+
+def make_data_per_class():
+    piano_paths = glob.glob('org_data/piano/*.jpg')
+    guitar_paths = glob.glob('org_data/guitar/*.jpg')
 
     piano_imgs = preprocess(piano_paths)
     guitar_imgs = preprocess(guitar_paths)
@@ -49,20 +93,21 @@ def make_data():
     pickle.dump(piano_norm, open('processed_data/piano_data.p', 'wb'))
     pickle.dump(guitar_norm, open('processed_data/guitar_data.p', 'wb'))
 
-    print('Success! :)')
+def view_data_by_type(type = 'p', start = 50, end = 55):
 
+    if type == 'p':
+        data = pickle.load(open('processed_data/piano_data.p', 'rb'))
+    else:
+        data = pickle.load(open('processed_data/guitar_data.p', 'rb'))
 
-make_data()
+    print(len(data))
 
+    for i in range(start, end):
+        arr = np.reshape(data[i], (28, 28))
+        plt.gray()
+        plt.imshow(arr)
+        plt.show()
 
-
-
-
-
-# quit()
-# print(arr)
-#
-
-# plt.gray()
-# plt.imshow(arr)
-# plt.show()
+data, labels = make_full_data()
+split_data(data, labels)
+# view_data_by_type()
