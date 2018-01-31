efcb9689ab17688b4579bb3553746f5414ceac0d
akench <atharva.kench@gmail.com>
2018-01-31 17:09:45 -0500

    organizes user's song library by emotion

--- projects/music_emotions/main.py
+++ projects/music_emotions/main.py
@@ -5,6 +5,9 @@ from make_spectrogram import graph_spectrogram
 from PIL import Image
 import numpy as np
 import utils.parse_img as parse_img
+import glob
+import numpy as np
+import time
 
 
 label_to_emot = {0:'happy', 1:'motivational', 2:'relaxing', 3:'angry', 4:'sad', 5:'tense'}
@@ -43,39 +46,39 @@ def percent(x):
 	print(perc)
 	return perc
 
-	# e = np.exp(np.array(x))
-	# dist = e / np.sum(e)
-	# print(dist)
-	# return dist
-
-
 def predict_song(spec_list):
 
 	label_list = [0, 0, 0, 0, 0, 0]
 
 	for img in spec_list:
-		
+
 		img = parse_img.resize_crop(img, size=299, grey = False)
 
 		lbl = inference(img)
-		print(lbl)
 		label_list[lbl] += 1
 
-	print(label_list)
-
-	return percent(label_list)
+	return label_list
 
 
-def make_output(percent_output):
+def print_results(output):
+
+	output = percent(output)
 
 	print('The song is:')
 
-	for i in range(len(percent_output)):
-		if percent_output[i] > 0.001:
-			print('%f percent %s' % (percent_output[i] * 100, label_to_emot[i]))
+	for i in range(len(output)):
+		if output[i] > 0.001:
+			print('%f percent %s' % (output[i] * 100, label_to_emot[i]))
 
 
+def emot_to_new_path(old_path, emot):
+	#old_path could be   dir/dir/dir/file.mp3
+	#file_name could be file.mp3
+	#prefix could be dir/dir/dir/
 
+	file_name = old_path.split('/')[-1]
+	prefix = old_path[: old_path.index(file_name)]
+	return prefix + emot + '/' + file_name
 
 
 
@@ -87,6 +90,8 @@ if __name__ == '__main__':
 	if choice == 'i':
 		audio = input('Enter a youtube link or filepath to an mp3\n')
 
+		t0 = time.time()
+
 		if 'https' in audio:
 			dl_audio_path = dl_audio(audio, None)
 			specs = graph_spectrogram(dl_audio_path, 10, save = False)
@@ -95,19 +100,30 @@ if __name__ == '__main__':
 			specs = graph_spectrogram(audio, 10, save = False)
 
 
-		sftmx = predict_song(specs)
-		make_output(sftmx)
+		perc = predict_song(specs)
+		print_results(perc)
+		print('took %f seconds to predict.' % (time.time() - t0))
 
-
-		
-
-		
 
 
 	elif choice == 'o':
 		directory = input('Enter filepath to directory of mp3 files')
 
+		t0 = time.time()
 
+		for emot in label_to_emot.values():
+			if not os.path.exists(directory + '/' + emot):
+				os.mkdir(directory + '/' + emot)
 
+		audio_files = glob.glob(directory + '/*.mp3')
 
+		for a in audio_files:
+			specs = graph_spectrogram(a, 10, save = False)
+			perc = predict_song(specs)
+			predicted_emot = label_to_emot[np.argmax(perc)]
 
+			new_path = emot_to_new_path(a, predicted_emot)
+
+			os.rename(a, new_path)
+
+		print('took %f seconds to organize.' % (time.time() - t0))
