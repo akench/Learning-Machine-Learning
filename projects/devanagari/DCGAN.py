2d9c7172d2f76ea0c7be2d2c09e0e86ba244e642
akench <atharva.kench@gmail.com>
2018-02-21 11:07:23 -0500

    .

--- projects/devanagari/DCGAN.py
+++ projects/devanagari/DCGAN.py
@@ -24,8 +24,8 @@ keep_prob_placeholder = tf.placeholder(dtype=tf.float32, shape=())
 
 
 def sample_Z(m, n):
-    # return np.random.uniform((size=m,n))
-    return np.random.normal(size=(m, n))
+    return np.random.uniform(size=(m,n))
+    # return np.random.normal(size=(m, n))
 
 
 def generator(Z, keep_prob):
@@ -37,6 +37,7 @@ def generator(Z, keep_prob):
              weights_regularizer=slim.l2_regularizer(.05)):
 
              net = slim.fully_connected(Z, 7*7*128, activation_fn=None, scope='fc1')
+             net = slim.dropout(net, keep_prob=keep_prob)
              net = slim.batch_norm(net)
              net = tf.nn.leaky_relu(net)
 
@@ -51,42 +52,6 @@ def generator(Z, keep_prob):
              print(net.shape)
 
              net = tf.nn.tanh(net)
-
-            # print('generator shapes')
-            # net = slim.fully_connected(Z, 256, activation_fn=None, scope='fc1')
-            # net = slim.dropout(net, keep_prob = keep_prob, scope='dropout1')
-            # net = slim.batch_norm(net)
-            # net = tf.nn.leaky_relu(net)
-            #
-            # net = slim.fully_connected(Z, 2*2*256, activation_fn=None, scope='fc2')
-            # net = slim.dropout(net, keep_prob = keep_prob, scope='dropout2')
-            #
-            # net = tf.reshape(net, (-1, 2, 2, 256))
-            # net = slim.batch_norm(net)
-            # net = tf.nn.leaky_relu(net)
-            # # shape = 2 x 2 x 256
-            # print(net.shape)
-            #
-            # net = slim.conv2d_transpose(net, 64, [2,2], stride=2, scope='convT1')
-            # net = slim.batch_norm(net)
-            # net = tf.nn.leaky_relu(net)
-            # # shape = 7 x 7 x 64
-            # print(net.shape)
-            #
-            # net = slim.conv2d_transpose(net, 16, [2,2], stride=2, scope='convT2')
-            # net = slim.batch_norm(net)
-            # net = tf.nn.leaky_relu(net)
-            # # shape = 15 x 15 x 16
-            # print(net.shape)
-            #
-            # net = slim.conv2d_transpose(net, 1, [2,2], stride=4, scope='convT3')
-            # net = slim.batch_norm(net)
-            # net = tf.nn.leaky_relu(net)
-            # # shape = 32 x 32 x 1
-            # print(net.shape)
-
-            # net = tf.nn.tanh(net)
-            # tf.summary.image('output of gen', net, 10)
 
     return net
 
@@ -112,14 +77,14 @@ def discriminator(X):
 
                 net = slim.conv2d(net, 64, [5,5], scope='conv1')
                 print(net.shape)
-                # net = slim.batch_norm(net)
+                net = slim.batch_norm(net)
                 net = tf.nn.leaky_relu(net)
                 net = slim.avg_pool2d(net, [2,2], stride=1, scope='pool1')
                 print(net.shape)
 
                 net = slim.conv2d(net, 128, [5,5], scope='conv2')
                 print(net.shape)
-                # net = slim.batch_norm(net)
+                net = slim.batch_norm(net)
                 net = tf.nn.leaky_relu(net)
                 net = slim.avg_pool2d(net, [2,2], stride=1, scope='pool2')
                 print(net.shape)
@@ -154,7 +119,7 @@ def train(continue_training=False):
 
     t0 = time.time()
 
-    generated = generator(Z, 0.5)
+    generated = generator(Z, keep_prob_placeholder)
     d_logit_real = discriminator(X)
     d_logit_fake = discriminator(generated)
 
@@ -194,7 +159,7 @@ def train(continue_training=False):
 
 
 
-    D_train_step = tf.train.AdamOptimizer(0.0001).minimize(D_loss, var_list=d_vars)
+    D_train_step = tf.train.AdamOptimizer(0.001).minimize(D_loss, var_list=d_vars)
     G_train_step = tf.train.AdamOptimizer(0.001).minimize(G_loss, var_list=g_vars)
 
 
@@ -232,7 +197,7 @@ def train(continue_training=False):
 
             #plot samples
             if it % 100 == 0:
-                samples = sess.run(generated, feed_dict={Z: seeds})
+                samples = sess.run(generated, feed_dict={Z: seeds, keep_prob_placeholder:1.0})
                 fig = plot_samples(samples)
                 fig.savefig('out/{}.png'.format(it), bbox_inches='tight')
 
@@ -250,7 +215,7 @@ def train(continue_training=False):
                 _, G_loss_curr, G_summary = sess.run([G_train_step, G_loss, summary_op],
                     feed_dict={Z: sample_Z(BATCH_SIZE, Z_DIM), X: batch_x, keep_prob_placeholder: .5})
 
-            if it % 2 == 0:
+            if it % 1 == 0:
                 _, D_loss_curr, D_summary = sess.run([D_train_step, D_loss, summary_op],
                     feed_dict={Z: sample_Z(BATCH_SIZE, Z_DIM), X: batch_x, keep_prob_placeholder: .5})
 
@@ -268,7 +233,7 @@ def train(continue_training=False):
 
 def gen_images(num):
 
-    generated = generator(Z)
+    generated = generator(Z, keep_prob_placeholder)
 
     with tf.Session() as sess:
         saver=tf.train.Saver()
@@ -286,4 +251,4 @@ def gen_images(num):
 
 
 # gen_images(50)
-train(continue_training=True)
+train(continue_training=False)
