f1fab169212cddc317a4b01452dcb2ff1bddf4b3
akench <atharva.kench@gmail.com>
2018-02-26 15:25:07 -0500

    working, but slow

--- projects/linear_regression/linreg.py
+++ projects/linear_regression/linreg.py
@@ -9,24 +9,25 @@ def plot_state(theta_0, theta_1, data, it):
 
 	plt.scatter(x_vals_real, y_vals_real)
 
-	x_vals_fake = np.linspace(0., np.max(x_vals_real))
+	x_vals_fake = np.linspace(0., np.max(x_vals_real) + 5)
 	y_vals_fake = [theta_0+theta_1*x for x in x_vals_fake]
 
 	plt.plot(x_vals_fake, y_vals_fake)
-	plt.axis([0, np.max(x_vals_real), -np.max(y_vals_real), np.max(y_vals_real)])
+	plt.axis([0, np.max(x_vals_real)+5, -np.max(y_vals_real) - 5, np.max(y_vals_real)+5])
 	plt.savefig('out/{}.png'.format(it), bbox_inches='tight')
 	plt.gcf().clear()
 
 
-def make_fake_data(num, slope):
+def make_fake_data(num, bias, slope, noise_factor):
 	min_x = 0.
 	max_x = 50.
 
 	data=[]
 	for _ in range(num):
 		x = np.random.uniform(min_x, max_x)
-		data.append((x, slope*x))
+		data.append((x, slope*x + bias + noise_factor*np.random.normal()))
 
+	np.random.shuffle(data)
 	return data
 
 
@@ -34,30 +35,27 @@ def make_fake_data(num, slope):
 # 1/2m * sum(f(x) - y)^2
 def calc_error(theta_0, theta_1, data):
 
-	error_sum = 0.
-	for x,y in data:
-		error_sum += ((theta_0 + theta_1*x) - y)**2
-
-
-	error = error_sum / (len(data) * 2)
+	error_sum = [((theta_0 + theta_1*x) - y)**2 for x,y in data]
+	error = np.sum(error_sum) / (len(data) * 2)
 	return error
 
 
 def update_vars(theta_0, theta_1, data, learning_rate):
 
-	deriv_0 = [(theta_0 + theta_1*x - y) for x,y in data]
+	deriv_0 = [((theta_0 + theta_1*x) - y) for x,y in data]
 	deriv_0	= np.sum(deriv_0) / len(data)
 
-	deriv_1 = [(theta_0 + theta_1*x - y)*x for x,y in data]
+	deriv_1 = [((theta_0 + theta_1*x) - y)*x for x,y in data]
 	deriv_1 = np.sum(deriv_1) / len(data)
 
 	theta_0 = theta_0 - learning_rate*deriv_0
 	theta_1 = theta_1 - learning_rate*deriv_1
 
-	return theta_0, theta_1
+	# print('deriv0=  {}      deriv1=  {}'.format(deriv_0, deriv_1))
+	return theta_0, theta_1, deriv_0, deriv_1
 
 
-def train(data = make_fake_data(num=50, slope=2), learning_rate = 0.001):
+def train(data = make_fake_data(num=50, bias=50, slope=-5, noise_factor=10), learning_rate = 0.001):
 
 	theta_0 = np.random.normal()
 	theta_1 = np.random.normal()
@@ -65,15 +63,23 @@ def train(data = make_fake_data(num=50, slope=2), learning_rate = 0.001):
 	error = calc_error(theta_0, theta_1, data)
 
 	it = 0
-	while error > 0.1:
-		error = calc_error(theta_0, theta_1, data)
-		print(error, 'is error')
-		plot_state(theta_0, theta_1, data, it)
+	deriv_1 = 10000
+	deriv_0 = 10000
+	plot_state(theta_0, theta_1, data, it)
+	while abs(deriv_0) + abs(deriv_1) > 0.001:
 
-		theta_0, theta_1 = update_vars(theta_0, theta_1, data, learning_rate)
+		# print('theta0 = {}  theta1= {}'.format(theta_0, theta_1))
+
+		error = calc_error(theta_0, theta_1, data)
+		# print('ERROR   ', error)
+
+		# plot_state(theta_0, theta_1, data, it)
+		theta_0, theta_1, deriv_0, deriv_1 = update_vars(theta_0, theta_1, data, learning_rate)
+		# print('')
 		it += 1
 
 	print('Line of best fit = {:0.4f}x + {:0.4f}'.format(theta_1, theta_0))
+	plot_state(theta_0, theta_1, data, it)
 
 
 train()
